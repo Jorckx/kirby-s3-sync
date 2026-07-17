cd5e4a5d1df93fad9bb6473d959bf904c2c8a225
Jore Dierckx <jore.dierckx@gmail.com>
2026-07-17 12:28:14 +0200

    autoloader removed for manual

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 
 use Kirby\Cms\App as Kirby;
 
+require_once __DIR__ . '/src/Client.php';
+require_once __DIR__ . '/src/Uploader.php';
+
 Kirby::plugin('joredierckx/kirby-s3-sync', [
     'options' => [
         'active' => false,
